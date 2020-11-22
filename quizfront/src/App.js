266361e3d4e7973da26bfec99bf7a8381598debf
shivam <shivamdatta465@gmail.com>
2020-11-23 00:52:30 +0530

    first commit

---
+++ quizfront/src/App.js
@@ -0,0 +1,18 @@
+import {Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
+import QuizSelect from './components/QuizSelect'
+import RandomQuiz from './components/RandomQuiz'
+
+
+function App() {
+  return (
+    <Router>
+        <Switch>
+          <Route path="/" component={QuizSelect} exact />
+          <Route path="/r/:topic" component={RandomQuiz} exact />
+        </Switch>
+    </Router>
+  );
+}
+
+export default App
---
+++ quizfront/src/components/framework/Header.js
@@ -0,0 +1,26 @@
+import { AppBar,Toolbar,Typography } from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+const useStyles=makeStyles((theme) =>({
+    root:{
+        flexGrow:1,
+    },
+    toolbarTitle: {
+        flexGrow: 1,
+      }
+}));
+
+export default function Header(){
+    const classes=useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" color="default" elevation={0} >
+            <Toolbar>
+                <Typography variant="h6" color="inherit"className={classes.toolbarTitle}>
+                    Shivam Quizzes
+                </Typography>
+            </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
